440bff4370ef3f7c0a19e6de36bbf4e6a8acd617
yuugo.ueno <y-ueno@olvdnet.co.jp>
2021-10-13 14:52:18 +0900

    ログインアクションの作成

--- src/main/java/actions/AuthAction.java
+++ src/main/java/actions/AuthAction.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 
+import actions.views.EmployeeView;
 import constants.AttributeConst;
 import constants.ForwardConst;
+import constants.MessageConst;
+import constants.PropertyConst;
 import services.EmployeeService;
 
 public class AuthAction extends ActionBase {
@@ -42,5 +45,46 @@ public class AuthAction extends ActionBase {
         //ログイン画面表示
         forward(ForwardConst.FW_LOGIN);
 
+    }
+
+    /*
+     * ログイン処理
+     */
+    public void login() throws ServletException,IOException{
+
+        String code = getRequestParam(AttributeConst.EMP_CODE);
+        String plainPass = getRequestParam(AttributeConst.EMP_PASS);
+        String pepper = getContextScope(PropertyConst.PEPPER);
+
+      //従業員の認証チェック
+        Boolean isValidEmployee = service.validateLogin(code, plainPass, pepper);
+
+        //認証チェック
+        if (isValidEmployee) {
+
+            //CSRF対策 tokenのチェック
+            if (checkToken()) {
+
+                //ログイン従業員のデータ取得
+                EmployeeView ev = service.findOne(code, plainPass, pepper);
+                //セッションにログインした従業員を設定
+                putSessionScope(AttributeConst.LOGIN_EMP, ev);
+                //セッションにログイン完了のフラッシュメッセージを設定
+                putSessionScope(AttributeConst.FLUSH, MessageConst.I_LOGINED.getMessage());
+
+                redirect(ForwardConst.ACT_TOP, ForwardConst.CMD_INDEX);
+            }
+        } else {
+
+            //CSRF対策用トークンを設定
+            putRequestScope(AttributeConst.TOKEN, getTokenId());
+            //認証失敗エラーメッセージ表示フラグをたてる
+            putRequestScope(AttributeConst.LOGIN_ERR, true);
+            //入力された従業員コードを設定
+            putRequestScope(AttributeConst.EMP_CODE, code);
+
+            //ログイン画面表示
+            forward(ForwardConst.FW_LOGIN);
+        }
     }
 }
