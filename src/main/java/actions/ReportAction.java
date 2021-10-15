5f709a3166b000d74ef019efd37f6cfe747a5857
yuugo.ueno <y-ueno@olvdnet.co.jp>
2021-10-15 10:27:23 +0900

    日報newアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -1,6 +1,7 @@
 package actions;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -54,6 +55,25 @@ public class ReportAction extends ActionBase {
         //一覧画面を表示
         forward(ForwardConst.FW_REP_INDEX);
 
+    }
+
+    /**
+     * 新規登録画面表示
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void entryNew() throws ServletException, IOException {
+
+        //CSRF対策用トークン
+        putRequestScope(AttributeConst.TOKEN, getTokenId());
+
+        //日報情報に今日の日付を設定
+        ReportView rv = new ReportView();
+        rv.setReportDate(LocalDate.now());
+        putRequestScope(AttributeConst.REPORT,rv);
+
+        //新規登録画面を表示
+        forward(ForwardConst.FW_REP_NEW);
     }
 
 }
